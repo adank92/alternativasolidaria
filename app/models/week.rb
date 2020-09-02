5c68ea1f89f0f7a718f2ff1769426df80b886b89
Adan Koch <adank92@gmail.com>
2020-09-02 00:29:35 -0300

    Refactored Week's name logic

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,12 +1,33 @@
 class Week < ApplicationRecord
   def self.for_current_month
     start = Date.today
-    @current_available_weeks = (start..start.end_of_month).select(&:on_weekend?).map(&:cweek).uniq.map do |week_number|
+    (start..start.end_of_month).select(&:on_weekend?).map(&:cweek).uniq.map do |week_number|
       create_or_find_by(year: start.year, number: week_number)
     end
   end
 
   def name
-    [I18n.l(Date.commercial(year, number, 6), format: :short), I18n.l(Date.commercial(year, number, 6), format: :short)].join(" - ")
+    [saturday, sunday].join(" - ")
+  end
+
+  private
+
+  SATURDAY = 6
+  SUNDAY = 7
+
+  def saturday
+    translate_day(SATURDAY)
+  end
+
+  def sunday
+    translate_day(SUNDAY)
+  end
+
+  def translate_day(date)
+    I18n.l(build_date(date), format: :short)
+  end
+
+  def build_date(day)
+    Date.commercial(year, number, day)
   end
 end
