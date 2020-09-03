2b6f5cea3aa2f0a8950058d46f5eeeffd72c3f06
Micaela Melek <micaelamelek@gmail.com>
2020-09-03 01:23:16 -0300

    added title attribute to the role icons

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
-  def role_icon(role)
-    return 'fas fa-pizza-slice' if role == :chef
-    'fas fa-truck'
+  def role_icon(role, index)
+    return ("<i class=\"fas fa-pizza-slice#{" ml-1" if index.equal?(1)}\" style=\"font-size: 1.5em\" title=\"Cocinero\"></i>").html_safe if role == :chef
+    ("<i class=\"fas fa-truck#{" ml-1" if index.equal?(1)}\" style=\"font-size: 1.5em\" title=\"Repartidor\"></i>").html_safe
   end
 end
