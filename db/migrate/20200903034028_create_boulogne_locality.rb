8efe65d725e2ce8a9072c36c2af2f67f571d2bb7
Adan Koch <adank92@gmail.com>
2020-09-05 19:03:01 -0300

    Fixed a migration that fails in Dokku

--- db/migrate/20200903034028_create_boulogne_locality.rb
+++ db/migrate/20200903034028_create_boulogne_locality.rb
@@ -1,7 +1,10 @@
 class CreateBoulogneLocality < ActiveRecord::Migration[6.0]
   def up
-    Province.where(name: "Buenos Aires").update_all(name: 'GBA')
-    department = Province.find_by(name: 'GBA').departments.create!(name: "Zona norte 1")
-    department.localities.create(name: 'Boulogne')
+    ba = Province.find_by(name: "Buenos Aires")
+    if ba.present?
+      ba.update(name: 'GBA')
+      department = ba.departments.create!(name: "Zona norte 1")
+      department.localities.create(name: 'Boulogne')
+    end
   end
 end
