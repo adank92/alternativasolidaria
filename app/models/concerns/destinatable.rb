924b3465ca051c782a000dcb7da9041392e96fe3
Adan Koch <adank92@gmail.com>
2020-10-02 00:51:14 -0300

    Delete all team destinations

--- app/models/concerns/destinatable.rb
+++ app/models/concerns/destinatable.rb
@@ -2,7 +2,7 @@ module Destinatable
   extend ActiveSupport::Concern
 
   included do
-    has_many :team_destinations
+    has_many :team_destinations, dependent: :destroy
     has_many :destinations, through: :team_destinations
 
     accepts_nested_attributes_for :team_destinations
